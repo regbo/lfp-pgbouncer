f1d2a869e1f0b9b6c5cbe0449745e3c050bf93ae
reggie pierce <reggie.pierce@gmail.com>
2022-08-15 17:11:27 -0400

    Auto JGit Commit 8/15/22 - 5:11 PM

--- app/src/main/java/com/lfp/pgbouncer_app/authenticator/AuthenticatorSigningKeyResolver.java
+++ app/src/main/java/com/lfp/pgbouncer_app/authenticator/AuthenticatorSigningKeyResolver.java
@@ -1,55 +1,39 @@
 package com.lfp.pgbouncer_app.authenticator;
 
-import java.util.Objects;
 import java.util.function.Predicate;
 
 import com.lfp.joe.core.classpath.Instances;
 import com.lfp.joe.core.properties.Configs;
-import com.lfp.joe.jwt.resolver.ClaimsPredicates;
-import com.lfp.joe.jwt.resolver.IssuerClaimSigningKeyResolver;
-import com.lfp.joe.utils.Utils;
+import com.lfp.joe.jwt.resolver.AbstractIssuerJwkSigningKeyResolver;
+import com.lfp.joe.jwt.resolver.ImmutableAbstractSigningKeyResolver.SigningKeyRequest;
+import com.lfp.joe.jwt.resolver.SigningKeyRequestFilters;
 import com.lfp.pgbouncer_app.config.PGBouncerAppConfig;
 
-import io.jsonwebtoken.Claims;
-import one.util.streamex.EntryStream;
-import one.util.streamex.StreamEx;
-
-public class AuthenticatorSigningKeyResolver extends IssuerClaimSigningKeyResolver {
+public class AuthenticatorSigningKeyResolver extends AbstractIssuerJwkSigningKeyResolver {
 
 	public static AuthenticatorSigningKeyResolver get() {
 		return Instances.get(AuthenticatorSigningKeyResolver.class);
 	}
 
+	private final Predicate<? super SigningKeyRequest> requestFilter;
+
 	protected AuthenticatorSigningKeyResolver() {
-		super(createClaimsPredicate());
+		var cfg = Configs.get(PGBouncerAppConfig.class);
+		Predicate<SigningKeyRequest> requestFilter = v -> false;
+		{
+			var predicate = SigningKeyRequestFilters.issuerHostsEquals(cfg.authenticatorJwtIssuerHosts());
+			requestFilter = requestFilter.or(predicate);
+		}
+		{
+			var predicate = SigningKeyRequestFilters.issuerURIMatches(cfg.authenticatorJwtIssuerURIs());
+			requestFilter = requestFilter.or(predicate);
+		}
+		this.requestFilter = requestFilter;
 	}
 
-	private static Predicate<Claims> createClaimsPredicate() {
-		var cfg = Configs.get(PGBouncerAppConfig.class);
-		Predicate<Claims> claimsPredicate = ClaimsPredicates.denyAll();
-		{
-			var predicate = ClaimsPredicates.fromIssuerHosts(cfg.authenticatorJwtIssuerHosts());
-			claimsPredicate = claimsPredicate.or(predicate);
-		}
-		{
-			var predicate = ClaimsPredicates.fromIssuerURIs(cfg.authenticatorJwtIssuerURIs());
-			claimsPredicate = claimsPredicate.or(predicate);
-		}
-		EntryStream<String, String> requiredClaims = Utils.Lots.streamMultimap(cfg.authenticatorJwtRequiredClaims());
-		requiredClaims = requiredClaims.filterKeys(Utils.Strings::isNotBlank);
-		requiredClaims = requiredClaims.mapValues(v -> v != null ? v : "");
-		for (var ent : requiredClaims) {
-			var requiredName = ent.getKey();
-			var requiredValue = ent.getValue();
-			claimsPredicate = claimsPredicate.and(claims -> {
-				var value = claims.get(requiredName);
-				StreamEx<Object> valueStream = Utils.Lots.tryStream(value).orElseGet(() -> StreamEx.of(value));
-				return valueStream.anyMatch(v -> {
-					return Objects.equals(requiredValue, v);
-				});
-			});
-		}
-		return claimsPredicate;
+	@Override
+	public Predicate<? super SigningKeyRequest> requestFilter() {
+		return requestFilter;
 	}
 
 }
